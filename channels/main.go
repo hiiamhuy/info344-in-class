bb666486a2ae777125ea94d26c05c189659900c1
Huy <hiiamhuy@uw.edu>
2017-05-03 14:27:23 -0700

    went over concurrency, files are channels and concur. Built a english dictonary searcher running concurrently

--- channels/main.go
+++ channels/main.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 //someLongFunc is a function that might
 //take a while to complete, so we want
 //to run it on its own go routine
-func someLongFunc() {
-
+func someLongFunc(ch chan int) {
+	r := rand.Intn(2000)
+	d := time.Duration(r)
+	time.Sleep(time.Millisecond * d)
+	ch <- r
 }
 
 func main() {
@@ -14,4 +23,18 @@ func main() {
 	//passing the channel so that
 	//someLongFunc() can communicate
 	//its results
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("starting long-running func...")
+	n := 10
+	//ch := make(chan int) //unbuffer channel
+	ch := make(chan int, n)
+	start := time.Now()
+	for i := 0; i < n; i++ { //write to ch
+		go someLongFunc(ch)
+	}
+	for i := 0; i < n; i++ { //read to ch
+		result := <-ch
+		fmt.Printf("result was %d\n", result)
+	}
+	fmt.Printf("took %v\n", time.Since(start))
 }
